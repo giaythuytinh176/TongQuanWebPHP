6d68657901a2b8ce63fde425c3ae4de05003d103
LE DUC TAM <giaythuytinh176@gmail.com>
2020-11-06 16:40:55 +0700

    app1

---
+++ AppCalculator1.php
@@ -0,0 +1,74 @@
+<html>
+<header>
+    <title>[Bài tập] Ứng dụng Calculator</title>
+    <style>
+
+        .inline {
+            display: inline-block;
+            margin: 3em;
+        }
+
+    </style>
+</header>
+
+<body>
+<br>
+<h1>Ứng dụng Calculator</h1>
+<br>
+<fieldset class="inline">
+    <legend>Calculator</legend>
+    <form method="POST">
+        First Operand <input type="text" id="first" name="FirstOperand"><br><br>
+        Second Operand <input type="text" id="second" name="SecondOperand"><br><br>
+        Operator
+        <input type="submit" name="btn" id="submit" value="+">
+        <input type="submit" name="btn" id="submit" value="-">
+        <input type="submit" name="btn" id="submit" value="*">
+        <input type="submit" name="btn" id="submit" value="/">
+        </select><br><br>
+    </form>
+</fieldset>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['FirstOperand']) && isset($_POST['SecondOperand']) && isset($_POST['btn'])) {
+        $FirstOperand = (float)$_POST['FirstOperand'];
+        $SecondOperand = (float)$_POST['SecondOperand'];
+        $operator = $_POST['btn'];
+        if ($operator == "+") {
+            $result = $FirstOperand + $SecondOperand;
+        } elseif ($operator == "-") {
+            $result = $FirstOperand - $SecondOperand;
+        } elseif ($operator == "*") {
+            $result = $FirstOperand * $SecondOperand;
+        } elseif ($operator == "/") {
+            if ($SecondOperand == 0) {
+                $result = "Số thứ 2 ko thể là 0. Mời nhập lại.";
+            } else $result = $FirstOperand / $SecondOperand;
+        }
+        ?>
+
+        <script>
+            document.getElementById("first").value = <?php echo $FirstOperand?>;;
+            document.getElementById("second").value = <?php echo $SecondOperand?>;;
+        </script>
+
+        <?php
+        echo "<h1>Results:  $FirstOperand $operator $SecondOperand = ";
+        echo $result;
+        echo "</h1>";
+    } else {
+        echo "Bạn chưa nhập.";
+    }
+
+}
+
+?>
+
+
+</body>
+
+
+</html>
+
